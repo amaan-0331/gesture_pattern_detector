ef7b6bfb3b1610a363ebdf0999a642c9de52f7aa
Amaan Ansari <masteramaan0331@gmail.com>
2024-09-09 16:56:20 +0400

    add behavior, supportedDevices, excludeFromSemantics, trackpadScrollCausesScale, dragStartBehavior, trackpadScrollToScaleFactor

--- lib/detector.dart
+++ lib/detector.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gesture_pattern_detector/gesture_pattern_detector.dart';
 
@@ -20,6 +21,14 @@ class GesturePatternDetector extends StatefulWidget {
     required this.child,
     this.timeout = const Duration(seconds: 5),
     super.key,
+
+    // default
+    this.behavior,
+    this.supportedDevices,
+    this.excludeFromSemantics = false,
+    this.trackpadScrollCausesScale = false,
+    this.dragStartBehavior = DragStartBehavior.start,
+    this.trackpadScrollToScaleFactor = kDefaultTrackpadScrollToScaleFactor,
   });
 
   /// The gesture pattern to detect.
@@ -41,6 +50,58 @@ class GesturePatternDetector extends StatefulWidget {
   ///
   /// If the pattern is not completed within this time frame, it will reset.
   final Duration timeout;
+
+  // default
+
+  /// How this gesture detector should behave during hit testing when deciding
+  /// how the hit test propagates to children and whether to consider targets
+  /// behind this one.
+  ///
+  /// This defaults to [HitTestBehavior.deferToChild] if [child] is not null and
+  /// [HitTestBehavior.translucent] if child is null.
+  ///
+  /// See [HitTestBehavior] for the allowed values and their meanings.
+  final HitTestBehavior? behavior;
+
+  /// Whether to exclude these gestures from the semantics tree. For
+  /// example, the long-press gesture for showing a tooltip is
+  /// excluded because the tooltip itself is included in the semantics
+  /// tree directly and so having a gesture to show it would result in
+  /// duplication of information.
+  final bool excludeFromSemantics;
+
+  /// Determines the way that drag start behavior is handled.
+  ///
+  /// If set to [DragStartBehavior.start], gesture drag behavior will
+  /// begin at the position where the drag gesture won the arena. If set to
+  /// [DragStartBehavior.down] it will begin at the position where a down event
+  /// is first detected.
+  ///
+  /// In general, setting this to [DragStartBehavior.start] will make drag
+  /// animation smoother and setting it to [DragStartBehavior.down] will make
+  /// drag behavior feel slightly more reactive.
+  ///
+  /// By default, the drag start behavior is [DragStartBehavior.start].
+  ///
+  /// Only the [DragGestureRecognizer.onStart] callbacks for the
+  /// [VerticalDragGestureRecognizer], [HorizontalDragGestureRecognizer] and
+  /// [PanGestureRecognizer] are affected by this setting.
+  ///
+  /// See also:
+  ///
+  ///  * [DragGestureRecognizer.dragStartBehavior], which gives an example for the different behaviors.
+  final DragStartBehavior dragStartBehavior;
+
+  /// The kind of devices that are allowed to be recognized.
+  ///
+  /// If set to null, events from all device types will be recognized. Defaults to null.
+  final Set<PointerDeviceKind>? supportedDevices;
+
+  /// {@macro flutter.gestures.scale.trackpadScrollCausesScale}
+  final bool trackpadScrollCausesScale;
+
+  /// {@macro flutter.gestures.scale.trackpadScrollToScaleFactor}
+  final Offset trackpadScrollToScaleFactor;
 
   @override
   State<GesturePatternDetector> createState() => _GesturePatternDetectorState();
@@ -120,6 +181,13 @@ class _GesturePatternDetectorState extends State<GesturePatternDetector> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      key: widget.key,
+      behavior: widget.behavior,
+      dragStartBehavior: widget.dragStartBehavior,
+      excludeFromSemantics: widget.excludeFromSemantics,
+      supportedDevices: widget.supportedDevices,
+      trackpadScrollCausesScale: widget.trackpadScrollCausesScale,
+      trackpadScrollToScaleFactor: widget.trackpadScrollToScaleFactor,
       onTap: _handleTap,
       onLongPress: _handleLongPress,
       onHorizontalDragEnd: (details) {
